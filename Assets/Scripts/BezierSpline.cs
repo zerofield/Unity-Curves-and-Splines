0f257487d52a5f97b67b592cce44444b1fddb78d
Jin Xu <xujin@i-vsun.com>
2016-07-12 18:25:06 +0800

    added spline

---
+++ Assets/Scripts/BezierSpline.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BezierSpline : MonoBehaviour
+{
+
+    public enum BezierControlPointMode
+    {
+        Free,
+        Aligned,
+        Mirrored
+    }
+
+    [SerializeField]
+    private Vector3[] points;
+
+    public int CurveCount
+    {
+        get
+        {
+            return (points.Length - 1) / 3;
+        }
+    }
+
+    public int ControlPointCount
+    {
+        get
+        {
+            return points.Length;
+        }
+    }
+
+    public Vector3 GetControlPoint(int index)
+    {
+        return points[index];
+    }
+
+    public void SetControlPoint(int index, Vector3 point)
+    {
+        points[index] = point;
+    }
+
+    public Vector3 GetPoint(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+        return transform.TransformPoint(Bezier.GetPoint(
+            points[i], points[i + 1], points[i + 2], points[i + 3], t));
+    }
+
+    public Vector3 GetVelocity(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+        return transform.TransformPoint(Bezier.GetFirstDerivative(
+            points[i], points[i + 1], points[i + 2], points[i + 3], t)) - transform.position;
+    }
+
+    public Vector3 GetDirection(float t)
+    {
+        return GetVelocity(t).normalized;
+    }
+
+    public void Reset()
+    {
+        points = new Vector3[]
+        {
+            new Vector3(1f, 0f, 0f),
+            new Vector3(2f, 0f, 0f),
+            new Vector3(3f, 0f, 0f),
+            new Vector3(4f, 0f, 0f)
+        };
+    }
+
+    public void AddCurve()
+    {
+        Vector3 point = points[points.Length - 1];
+        Array.Resize(ref points, points.Length + 3);
+        point.x += 1;
+        points[points.Length - 3] = point;
+        point.x += 1;
+        points[points.Length - 2] = point;
+        point.x += 1;
+        points[points.Length - 1] = point;
+    }
+
+  
+
+}
